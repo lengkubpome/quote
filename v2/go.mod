4c53cf33d49c694e984a383aa07ca2f12e199a63
lengkubpome <lengkubpome@gmail.com>
2021-02-28 01:33:45 +0700

    update v2

---
+++ v2/go.mod
@@ -0,0 +1,3 @@
+module github.com/lengkubpome/quote/v2
+
+go 1.16
---
+++ v2/quote.go
@@ -0,0 +1,11 @@
+package quote
+
+// Say : Hello
+func Say2() string {
+	return "Hello"
+}
+
+// Speak : Hi, Mike
+func Speak2() string {
+	return "Hi, Mike"
+}
